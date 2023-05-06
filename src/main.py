cafc06680d72109f88215ce40c0de7191871da97
lycheejam <lycheejamwow@gmail.com>
2023-05-06 12:35:03 +0900

    やっていき

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from sendgrid import SendGridAPIClient
 from dotenv import load_dotenv
 import json
 from models.teammate import Teammate
-import pprint  # noqa: F821
-import datetime
+import pprint
+import argparse
 
 load_dotenv()
 
@@ -15,42 +15,62 @@ class SendgridTeammatesManage:
         self.sg = self.authorize_client()
 
     def main(self) -> None:
-        timestamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+
+        parser = argparse.ArgumentParser(description="Sendgridのteammatesを管理するスクリプト。")
+        parser.add_argument(
+            "command", choices=["invite", "delete", "describe"], help="実行する処理を指定してください。"
+        )
+        parser.add_argument(
+            "--email",
+            type="str",
+            nargs="?",
+            default=None,
+            help="inviteまたはdeleteの場合、emailを指定してください。",
+        )
+        parser.add_argument(
+            "--role",
+            type="str",
+            nargs="?",
+            default=None,
+            help="inviteの場合、roleを指定してください。",
+        )
+
+        args = parser.parse_args()
+
+        if not self.validate_options(args):
+            return
+
         env = os.environ.get("env", "")
-
-        before_fetch_results = self.fetch_teammates()
-        file_name = "_".join(filter(None, (timestamp, env, "before.json")))
-        self.create_results_json(before_fetch_results, file_name)
-
-        for result in before_fetch_results:
-            if result["pending_token"] is not None:
-                self.delete_pending_teammate(result["pending_token"])
-
-        users = self.read_json_to_dict("/".join(filter(None, (env, "users.json"))))
         roles = self.read_json_to_dict("/".join(filter(None, (env, "roles.json"))))
 
-        for user in users:
-            for result in before_fetch_results:
-                if user["email"] == result["email"] and user["state"] == "absent":
-                    self.delete_teammate(result["username"])
-
-        resulet_emails = [result["email"] for result in before_fetch_results]
-        for user in users:
-            if user["email"] not in resulet_emails and user["state"] == "present":
-                self.invite_teammate(user["email"], roles[user["roles"]])
-
-        after_fetch_results = self.fetch_teammates()
-        file_name = "_".join(filter(None, (timestamp, env, "after.json")))
-        self.create_results_json(after_fetch_results, file_name)
+        if args.command == "invite":
+            # invite保留中のユーザを確認して今回invite対象のユーザと重複していた場合に削除
+            pending_teammates = self.get_pending_teammates()
+            if pending_teammates is not None:
+                for pending_teammate in pending_teammates:
+                    if pending_teammate["email"] == args.email:
+                        self.delete_pending_teammate(pending_teammate["pending_token"])
+            self.invite_teammate(args.email, roles[args.role])
+        elif args.command == "delete":
+            self.delete_teammate(args.email)
+        elif args.command == "describe":
+            pprint.pprint(json.dump(self.fetch_teammates()))
 
     def authorize_client(self) -> SendGridAPIClient:
         return SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
 
-    def get_teammates(self):
-        try:
-            return self.sg.client.teammates.get().to_dict["result"]
-        except Exception:
-            pprint.pprint(traceback.format_exc())
+    def validate_options(self, args):
+        if args.command == "invite" and args.email is None:
+            pprint.pprint("inviteを実行する場合、emailを指定してください。")
+            return False
+        if args.command == "invite" and args.role is None:
+            pprint.pprint("inviteを実行する場合、roleを指定してください。")
+            return False
+        if args.command == "delete" and args.email is None:
+            pprint.pprint("deleteを実行する場合、emailを指定してください。")
+            return False
+
+        return True
 
     def get_pending_teammates(self):
         try:
@@ -58,21 +78,9 @@ class SendgridTeammatesManage:
         except Exception:
             pprint.pprint(traceback.format_exc())
 
-    def get_teammate_scopes(self, username):
-        try:
-            return self.sg.client.teammates._(username).get().to_dict["scopes"]
-        except Exception:
-            pprint.pprint(traceback.format_exc())
-
     def delete_pending_teammate(self, token):
         try:
             return self.sg.client.teammates.pending._(token).delete()
-        except Exception:
-            pprint.pprint(traceback.format_exc())
-
-    def delete_teammate(self, username):
-        try:
-            return self.sg.client.teammates._(username).delete()
         except Exception:
             pprint.pprint(traceback.format_exc())
 
@@ -83,13 +91,21 @@ class SendgridTeammatesManage:
         except Exception:
             pprint.pprint(traceback.format_exc())
 
-    def create_results_json(self, results, file_name):
+    def get_teammates(self):
         try:
-            file_path = os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), "../logs", file_name
-            )
-            with open(file_path, "w") as f:
-                json.dump(results, f, indent=4)
+            return self.sg.client.teammates.get().to_dict["result"]
+        except Exception:
+            pprint.pprint(traceback.format_exc())
+
+    def get_teammate_scopes(self, username):
+        try:
+            return self.sg.client.teammates._(username).get().to_dict["scopes"]
+        except Exception:
+            pprint.pprint(traceback.format_exc())
+
+    def delete_teammate(self, username):
+        try:
+            return self.sg.client.teammates._(username).delete()
         except Exception:
             pprint.pprint(traceback.format_exc())
 
